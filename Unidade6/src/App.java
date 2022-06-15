af3aa2f4c520bdfbae2b04be45fa2bd2cf45636b
felipeaugustus <felipe.a.dc.araujo@gmail.com>
2022-06-14 21:49:00 -0300

    uni 10 e bubble

--- Unidade6/src/App.java
+++ Unidade6/src/App.java
@@ -1,5 +1,31 @@
 public class App {
+    private App() {
+        int vet[] = new int[4];
+        vet[0] = 10; vet[1] = 40; vet[2] = 5; vet[3] = 30;
+        int bubble;  
+        //for (int i = 0; i < vet.length - 1; i++) {
+        //    if (vet[i] > vet[i+1]) {
+        //        bubble = vet[i];
+        //        vet[i] = vet[i+1];
+        //        vet[i+1] = bubble;
+        //        i = -1;
+        //    }
+        //}
+        for (int i = 0; i < vet.length-1; i++) {
+            for (int j = 0; j < vet.length-i -1; j++) {
+                if (vet[j]> vet[j+1]) {
+                    bubble = vet[j];
+                    vet[j] = vet[j+1];
+                    vet[j+1] = bubble;
+                }
+            }
+        }   
+        for (int i = 0; i < vet.length; i++) {
+            System.out.println(vet[i]);
+        }
+    }
+    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        new App();
     }
 }
--- Unidade6/src/Uni06Exe10.java
+++ Unidade6/src/Uni06Exe10.java
@@ -1,13 +1,127 @@
+import java.util.Arrays;
 import java.util.Scanner;
+
 public class Uni06Exe10 {
     Uni06Exe10() {
         Scanner tec = new Scanner(System.in);
-        int menuNumber[] = new int[50];        
+        int vetMenu[] = new int[10];
+        boolean isProgramRunning = true;
+        int n = 0;
         do {
-            System.out.println(String.format("1-Incluir valor %n 2-Pesquisar valor %n 3-Alterar valor %n 4-Excluir valor %n 5-Mostrar valores %n 6-Ordenar valores %n 7-Inverter valores %n 8-Sair do sistema"));
-            
-        } while (true);
+            System.out.println(String.format(
+                    "1-Incluir valor %n 2-Pesquisar valor %n 3-Alterar valor %n 4-Excluir valor %n 5-Mostrar valores %n 6-Ordenar valores %n 7-Inverter valores %n 8-Sair do sistema"));
+            n = tec.nextInt();
+            switch (n) {
+                case 1:
+                IncluirValor(vetMenu, tec);
+                    break;
+                case 2:
+                PesquisarValor(vetMenu, tec);
+                    break;
+                case 3:
+                AlterarValor(vetMenu, tec);
+                    break;
+                case 4:
+                ExcluirValor(vetMenu, tec);
+                    break;
+                case 5:
+                MostrarValores(vetMenu);
+                    break;
+                case 6:
+                OrdenarValores(vetMenu);
+                    break;
+                case 7:
+
+                    break;
+                case 8:
+                isProgramRunning = Encerrar();
+                    break;
+                default:
+                    System.out.println("Opção invalida");
+                    break;
+            }
+        } while (isProgramRunning = true);
     }
+    void IncluirValor(int vetMenu[], Scanner tec) {
+        int numero = tec.nextInt();
+        if (vetMenu[vetMenu.length-1] == 0) {
+            vetMenu[vetMenu.length-1] = numero;
+            System.out.println("numero inserido");
+        }
+        else {
+            System.out.println("numero não inserido");
+        }
+        
+    }
+    void PesquisarValor(int vetMenu[], Scanner tec) {
+        int pesquisa = tec.nextInt();
+        boolean aparece = false;
+        for (int i = 0; i < vetMenu.length; i++) {
+            if (vetMenu[i] == pesquisa) {
+                System.out.println("O valor consta na posição: " + i);
+                aparece = true;
+            }
+        }
+        if (aparece = false) {
+            System.out.println("O valor não consta no vetor");
+        }
+    }
+    void AlterarValor(int vetMenu[], Scanner tec) {
+        System.out.println("Insira um numero a ser trocado");
+        int alterado = tec.nextInt();
+        System.out.println("Insira o que entrara no lugar do anterior");
+        int novo = tec.nextInt();
+        boolean trocou = false;
+        for (int i = 0; i < vetMenu.length; i++) {
+            if (vetMenu[i] == alterado && trocou == false) {
+                vetMenu[i] = novo;
+                trocou = true;
+                System.out.println("O numero na posição [" + i + "] agora é: " + novo);
+            }
+        }
+        if (trocou == false) {
+            System.out.println("O numero indicado não consta no vetor");
+        }
+
+    }
+    void ExcluirValor(int vetMenu[], Scanner tec) {
+        int excluido = tec.nextInt();
+        boolean operacaoConcluida = false;
+        for (int i = 0; i < vetMenu.length ; i++) {
+            if (vetMenu[i] == excluido && operacaoConcluida) {
+                for (int j = 0; j < vetMenu.length - i -1; j++) {
+                    {
+                        vetMenu[i+j] = vetMenu[i+j+1];
+                        operacaoConcluida = true;
+                    }
+                    
+                }
+                System.out.println("Numero removido");
+            }
+        }
+        if (operacaoConcluida == false) {
+            System.out.println("O numero escolhido não consta no vetor");
+        }
+    }
+    void MostrarValores(int vetMenu[]) {
+        for (int i = 0; i < vetMenu.length; i++) {
+            if (vetMenu[i] != 0) {
+                System.out.println(vetMenu[i]);
+            }
+        }
+    }
+    void OrdenarValores(int vetMenu[]) {
+        Arrays.sort(vetMenu);
+        System.out.println("Vetor ordenado");
+    }
+    
+
+
+
+    boolean Encerrar() {
+        return false;
+    }
+
     public static void main(String[] args) {
         new Uni06Exe10();
     }
